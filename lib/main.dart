72ce1f6a564beabef2a345454b31abb8f75a3b4c
criscoelhor <coelhocr@gmail.com>
2019-10-07 23:20:20 -0300

    add same page controls

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main(){
+void main() {
+  Firestore.instance
+      .collection("teste")
+      .document("testeDocument")
+      .setData({"teste": "teste"});
 
-  Firestore.instance.collection("teste").document("testeDocument").setData({"teste" : "teste"});
-
- runApp(MyApp());
+  runApp(MyApp());
 }
 
 final ThemeData kIOSTheme = ThemeData(
@@ -14,12 +16,85 @@ final ThemeData kIOSTheme = ThemeData(
   primaryColorBrightness: Brightness.light,
 );
 
+final ThemeData kDefaultTheme = ThemeData(
+    primarySwatch: Colors.purple,
+    accentColor: Colors.orangeAccent[400]
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Chat App",
-      debugShowCheckedModeBanner: false,
+        title: "Chat App",
+        debugShowCheckedModeBanner: false,
+        theme: Theme
+            .of(context)
+            .platform == TargetPlatform.iOS ?
+        kIOSTheme : kDefaultTheme,
+        home: ChatScreen()
     );
   }
 }
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Chat App"),
+          centerTitle: true,
+          elevation: Theme
+              .of(context)
+              .platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Theme
+                    .of(context)
+                    .cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+        data: IconThemeData(color: Theme
+            .of(context)
+            .accentColor),
+        child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          decoration: Theme
+              .of(context)
+              .platform == TargetPlatform.iOS ?
+          BoxDecoration(
+            border: Border(top: BorderSide(color: Colors.grey[200]))
+          ) :
+          null,
+        )
+    );
+  }
+}
+
